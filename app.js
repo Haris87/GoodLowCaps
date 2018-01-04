1485785fa0f2144c92339154a2fe3177e0065c28
xbouchlis <x.bouchlis@atc.gr>
2018-01-04 14:34:28 +0200

    refactoring, added comments and logs

--- app.js
+++ app.js
@@ -1,34 +1,71 @@
 var coinmarketcap = require('./coinmarketcap');
 var fs = require('fs');
+var exec = require('child_process').exec;
 
-coinmarketcap.getCoins({start: 0, limit: 1000}, function(coins){
-	var goodCoins = coins.filter(isGoodCoin);
-	saveToFile(goodCoins);
-});
+
+// Change these parameters to suite your search
+var params = {
+	highestMarketCap: 250000, 			// coins that have up to that much market cap ($), default: 250,000
+	lowestMarketCap: 50000, 			// coins that have at least that much market cap ($), default: 50,000
+	maxTotalSupply: 50000000, 			// total supply of coins should not be more than this number, default: 50,000,000
+	minTotalAvailableSupplyRatio: 0.8, 	// ratio of available coins compared to total supply of coins, default: 0.8
+	minVolume24hMarketCapRatio: 0.02 	// 24h volume as percentage of market cap should be at least this much, default: 0.02
+}
+
+function run(){
+	logParams();
+	// get coin listings
+	coinmarketcap.getCoins({start: 0, limit: 1000}, function(coins){
+		// save coins that meet given parameters to output file
+		saveToFile(coins.filter(isGoodCoin));
+	});
+};
+run();
+
+function logParams(){
+	console.log("Parameters:");
+	console.log("Market cap below: \n>", params.highestMarketCap);
+	console.log("Market cap above: \n>", params.lowestMarketCap);
+	console.log("Total supply below: \n>", params.maxTotalSupply);
+	console.log("Total supply to available supply ratio above: \n>", params.minTotalAvailableSupplyRatio);
+	console.log("24h volume to market cap ration above: \n>", params.minVolume24hMarketCapRatio);
+};
 
 function isGoodCoin(coin){
-	var marketCapLessThan250k = coin.market_cap_usd < 250000;
-	var marketCapMoreThan50k = coin.market_cap_usd > 50000;
-	var supplyLessThan50m = coin.total_supply < 50000000;
-	var moreThan2percent24hVolumeToCap = coin['24h_volume_usd']/coin.market_cap_usd > 0.02;
-	var totalSupplyCloseToAvailableSupply = coin.available_supply/coin.total_supply > 0.8; 
-	
-	if(	marketCapLessThan250k && 
-		marketCapMoreThan50k && 
-		supplyLessThan50m && 
-		moreThan2percent24hVolumeToCap && 
-		totalSupplyCloseToAvailableSupply) {
-		
+	if(coinMeetsConditions(coin, params)) {
+		//add link to coin
+		coin.link = "https://coinmarketcap.com/currencies/"+coin.id;
+		console.log('Coin added:', coin.name, coin.symbol);
 		return coin;
 	}
-}
+};
+
+function coinMeetsConditions(coin, params){
+	var maxMarketCap = coin.market_cap_usd < params.highestMarketCap;
+	var minMarketCap = coin.market_cap_usd > params.lowestMarketCap;
+	var totalSupply = coin.total_supply < params.maxTotalSupply;
+	var _24hMarketCapRatio = coin['24h_volume_usd']/coin.market_cap_usd > params.minVolume24hMarketCapRatio;
+	var totalAvailableSupplyRatio = coin.available_supply/coin.total_supply > params.minTotalAvailableSupplyRatio; 
+	
+	return maxMarketCap && 
+		minMarketCap && 
+		totalSupply && 
+		_24hMarketCapRatio && 
+		totalAvailableSupplyRatio;
+};
 
 function saveToFile(coins) {
 	var outputfile = 'goodcoins.json';
-	fs.writeFile(outputfile, JSON.stringify(coins), function(err) {
+	var contents = JSON.stringify({
+		"params": params,
+		"coins": coins
+	});
+	
+	fs.writeFile(outputfile, contents, function(err) {
 		if(err) {
 			return console.log(err);
 		}
 		console.log("Coins saved: ", coins.length);
+		exec(outputfile);
 	}); 
-}
+};
--- coinmarketcap.js
+++ coinmarketcap.js
@@ -2,14 +2,17 @@ var request = require('request');
 
 function getCoins(options, successCallback, errorCallback){
 	
-	var start = options.start || '200';
+	var start = options.start || '0';
 	var limit = options.limit || '100';
 	var url = 'https://api.coinmarketcap.com/v1/ticker/?start='+start+'&limit='+limit;
 	
+	console.log("Retrieving coin list from coinmarketcap...");
 	request.get(url, { json:true }, function(err, res, body){
 		if(err) {
+			console.log("Error getting coin list from coinmarketcap:", err);
 			errorCallback(err);
 		} else {
+			console.log("Coin list retrieved from coinmarketcap.");
 			successCallback(body);
 		}
 	});
